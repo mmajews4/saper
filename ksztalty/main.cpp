cf8704745b1df5f2125a8e5d745573a99de6b974
Mateusz Majewski <278292@student.pwr.edu.pl>
2024-04-16 07:29:52 +0200

    Gotowa plansza bez numerków

--- ksztalty/main.cpp
+++ ksztalty/main.cpp
@@ -1,23 +1,36 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+// g++ main.cpp -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+
 
 int main()
 {
-    int height = 9, width = 9;
-    float tile_size = 50, distance = 10, offset;
+    int window_width = 800, window_height = 600;
+    int height = 9, width = 15;
+    float tile_size = 50, spacing = 3, left_offset = 10, top_offset = 10, tile_frame = 3;
 
+    // Wycentrowanie planszy
+    left_offset = (window_width - (width*(tile_size + (tile_frame * 2)+ spacing))) / 2;
+    top_offset = (window_height - (height*(tile_size + (tile_frame * 2)+ spacing))) / 2;
 
+    // Zabezpieczenie przed ustawieniem zbyt dużej planszy
+    if(left_offset < 0 || top_offset < 0){
+        std::cout << "Window too small to display board" << std::endl;
+        return 0;
+    }
 
     // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Saper");
 
     // define a 120x50 rectangle
     sf::RectangleShape rectangle(sf::Vector2f(tile_size, tile_size));
 
     // change the size to 100x100
-    rectangle.setFillColor(sf::Color(100, 250, 50));
+    rectangle.setFillColor(sf::Color(220, 220, 220));
     // set a 10-pixel wide orange outline
-    rectangle.setOutlineThickness(5);
-    rectangle.setOutlineColor(sf::Color(250, 150, 100));
+    rectangle.setOutlineThickness(tile_frame);
+    rectangle.setOutlineColor(sf::Color(120, 120, 120));
 
 
 
@@ -42,16 +55,13 @@ int main()
         // clear the window with black color
         window.clear(sf::Color::Black);
 
-        // draw everything here...
+      // draw everything here...
         for(int row = 0; row < width; row++){
             for(int col = 0; col < height; col++){
-                rectangle.setPosition(, 350);
+                rectangle.setPosition(col * (tile_size + spacing + (tile_frame * 2)) + left_offset, row * (tile_size + spacing + (tile_frame * 2)) + top_offset);
                 window.draw(rectangle);
             }
         }
-
-        rectangle.setPosition(300, 350);
-        window.draw(rectangle);
 
 
         // end the current frame
