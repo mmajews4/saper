c80f804a07d7df6a006b11cfc3da1dd921199f7e
Mateusz Majewski <278292@student.pwr.edu.pl>
2024-04-16 13:00:21 +0200

    sfml planszy w oddzielnym pliku gotowy

--- ksztalty/main.cpp
+++ ksztalty/main.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
+#include <cmath>
 
 // g++ main.cpp -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
 
@@ -7,10 +9,14 @@
 int main()
 {
     int window_width = 800, window_height = 600;
-    int height = 9, width = 15;
-    float tile_size = 50, spacing = 3, left_offset = 10, top_offset = 10, tile_frame = 3;
+    int height = 9, width = 9;
+    double tile_size = 50, spacing = 3, left_offset = 10, top_offset = 10, tile_frame = 3;
+    int text_left_offset = 13, text_top_offset = 5;
+    int tile_press_row, tile_press_col;
 
-    // Wycentrowanie planszy
+    std::string tile_text = "8";
+
+    // Wycentrowanie planszy 
     left_offset = (window_width - (width*(tile_size + (tile_frame * 2)+ spacing))) / 2;
     top_offset = (window_height - (height*(tile_size + (tile_frame * 2)+ spacing))) / 2;
 
@@ -23,16 +29,27 @@ int main()
     // create the window
     sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Saper");
 
-    // define a 120x50 rectangle
+    // create rectangle object
     sf::RectangleShape rectangle(sf::Vector2f(tile_size, tile_size));
 
-    // change the size to 100x100
     rectangle.setFillColor(sf::Color(220, 220, 220));
-    // set a 10-pixel wide orange outline
     rectangle.setOutlineThickness(tile_frame);
     rectangle.setOutlineColor(sf::Color(120, 120, 120));
 
 
+
+    // Load a font
+    sf::Font font;
+    if(!font.loadFromFile("arial.ttf")) {
+        return EXIT_FAILURE;
+    }
+
+    // Create a text object
+    sf::Text text;
+    text.setFont(font);
+    text.setStyle(sf::Text::Bold);
+    text.setCharacterSize(tile_size - (2 * text_top_offset));
+    text.setFillColor(sf::Color(200, 0, 0));
 
 
 /*
@@ -41,25 +58,51 @@ int main()
     shape.setTextureRect(sf::IntRect(10, 10, 100, 100));
 */
     // run the program as long as the window is open
-    while (window.isOpen())
+    while(window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (window.pollEvent(event))
+        while(window.pollEvent(event))
         {
             // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
+            if(event.type == sf::Event::Closed)
                 window.close();
+            else if(event.type == sf::Event::MouseButtonPressed)
+            {
+                // calculate on wich tile action was taken, floor to not count -0.xx as 0
+                tile_press_col = floor((event.mouseButton.x - left_offset)/(tile_size + spacing + (tile_frame * 2)));
+                tile_press_row = floor((event.mouseButton.y - top_offset)/(tile_size + spacing + (tile_frame * 2)));
+
+                if(!(tile_press_col >= 0 && tile_press_col < width && tile_press_row >= 0 && tile_press_row < height))
+                {
+                    std::cout << "Press out of board" << std::endl;
+                }
+                else if(event.mouseButton.button == sf::Mouse::Left)
+                {   
+                    std::cout << "Reveal (" << tile_press_col << "," << tile_press_row << ")" << std::endl;
+                } 
+                else if(event.mouseButton.button == sf::Mouse::Right)
+                {
+                    std::cout << "Place flag (" << tile_press_col << "," << tile_press_row << ")" << std::endl;
+                }
+            }
         }
 
         // clear the window with black color
         window.clear(sf::Color::Black);
 
-      // draw everything here...
+
+        // Set the position of the text
+        text.setString(tile_text);
+        text.setPosition(100, 100);
+
+        // draw everything here...
         for(int row = 0; row < width; row++){
             for(int col = 0; col < height; col++){
                 rectangle.setPosition(col * (tile_size + spacing + (tile_frame * 2)) + left_offset, row * (tile_size + spacing + (tile_frame * 2)) + top_offset);
+                text.setPosition(col * (tile_size + spacing + (tile_frame * 2)) + left_offset + text_left_offset, row * (tile_size + spacing + (tile_frame * 2)) + top_offset);
                 window.draw(rectangle);
+                window.draw(text);
             }
         }
 
